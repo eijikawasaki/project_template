8b591eefffee003d60f1a219ade5dad303f13cb4
Eiji Kawasaki <eijikawasaki@aol.com>
2018-12-06 14:31:05 +0100

    Initial commit

--- src/core.py
+++ src/core.py
@@ -1,11 +1,13 @@
-class ClassTeplate():
-	"""
-	Class' comments
-	""""
-	Attribute = True
-	def hello_wormd():
-		"""
-		Method's comments
-		"""
-		print("Hello world")
+class ClassTemplate:
+    """
+    Class' comments
+    """
+    Attribute = True
+
+    def hello_world(self):
+        """
+        Method's comments
+        """
+        self.Attribute = False
+        print("Hello world")
 
